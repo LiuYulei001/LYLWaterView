6091df51d51ad9fd880db92d47e1d150d4777004
LYL <445229678@qq.com>
2016-05-24 15:14:57 +0800

    framework

---
+++ LYLWaterView.framework/Headers/DoubleWaterView.h
@@ -0,0 +1,22 @@
+//
+//  DoubleWaterView.h
+//  WaterView
+//
+//  Created by SPI-绿能宝 on 16/4/7.
+//  Copyright © 2016年 MyMac. All rights reserved.
+//
+
+//#define kWaterViewColler kBorderLineColler
+//#define kWaterView1Coller kBorderLineColler
+
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface DoubleWaterView : UIView
+
+@property (nonatomic,assign) CGFloat percentNum;
+
+
+@end
---
+++ LYLWaterView.framework/Headers/LYLWaterView.h
@@ -0,0 +1,21 @@
+//
+//  LYLWaterView.h
+//  LYLWaterView
+//
+//  Created by SPI-绿能宝 on 16/5/24.
+//  Copyright © 2016年 北京SPI绿能宝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DoubleWaterView.h"
+#import "UConstants.h"
+#import "WaterView.h"
+//! Project version number for LYLWaterView.
+FOUNDATION_EXPORT double LYLWaterViewVersionNumber;
+
+//! Project version string for LYLWaterView.
+FOUNDATION_EXPORT const unsigned char LYLWaterViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LYLWaterView/PublicHeader.h>
+
+
